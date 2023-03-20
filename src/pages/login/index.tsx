2168e18f2ffd56e65e28e854ed3c46a4b936cd02
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-03-20 22:34:16 +0200

    fix: fix error require.e is not a function

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,10 @@
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
-const HankoAuth = dynamic(() => import("../../components/auth/HankoAuth"), { ssr: false });
 
 export default function Home() {
-  return <Suspense fallback="Loading ...">
+  const HankoAuth = dynamic(() => import("../../components/auth/HankoAuth"), { ssr: false });
+
+  return <Suspense fallback="Loading...">
     <HankoAuth />
   </Suspense >;
 }
