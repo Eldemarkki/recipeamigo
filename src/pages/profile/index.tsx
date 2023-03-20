d418407eb6c13dea4dca7d527d70a9e151f95b58
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-03-20 22:34:32 +0200

    fix: only show profile page is user is logged in

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,29 @@
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
-const HankoProfile = dynamic(() => import("../../components/auth/HankoProfile"), { ssr: false });
+import { getUserFromRequest } from "../../utils/auth";
+import { GetServerSideProps } from "next";
 
 export default function ProfilePage() {
+  const HankoProfile = dynamic(() => import("../../components/auth/HankoProfile"), { ssr: false });
+
   return <Suspense fallback="Loading...">
     <HankoProfile />
   </Suspense>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const user = await getUserFromRequest(req);
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
