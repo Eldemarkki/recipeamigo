65834d8a584aa3309ebf4815956d8efb9709f2e0
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-03-20 22:29:33 +0200

    feat: show status 401 if user is unauthorized in /api/user

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -2,7 +2,10 @@ import { NextApiHandler } from "next";
 import { getUserFromRequest } from "../../utils/auth";
 
 const handler: NextApiHandler = async (req, res) => {
-  const user = getUserFromRequest(req);
+  const user = await getUserFromRequest(req);
+  if (!user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 
   return res.json(user);
 };
