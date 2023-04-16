b14f2fe9d0e04afed446a80f57fb10afbe1a1671
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-04-16 14:13:33 +0300

    test: add more tests for getAllRecipesForUser

--- src/database/recipes.integration.test.ts
+++ src/database/recipes.integration.test.ts
@@ -16,4 +16,25 @@ describe("recipes", () => {
     expect(recipes).toHaveLength(1);
     expect(recipes[0]).toEqual(newRecipe);
   });
+
+  it("should return array with 2 elements when user has 2 recipes", async () => {
+    const userId = await createUserToDatabase();
+    await createRecipe(userId, createRandomRecipe());
+    await createRecipe(userId, createRandomRecipe());
+    const recipes = await getAllRecipesForUser(userId);
+    expect(recipes).toHaveLength(2);
+  });
+
+  it("shouldn't return other users' recipes", async () => {
+    const userId1 = await createUserToDatabase();
+    const userId2 = await createUserToDatabase();
+    const newRecipe1 = await createRecipe(userId1, createRandomRecipe());
+    const newRecipe2 = await createRecipe(userId2, createRandomRecipe());
+    const recipes1 = await getAllRecipesForUser(userId1);
+    const recipes2 = await getAllRecipesForUser(userId2);
+    expect(recipes1).toHaveLength(1);
+    expect(recipes2).toHaveLength(1);
+    expect(recipes1[0]).toEqual(newRecipe1);
+    expect(recipes2[0]).toEqual(newRecipe2);
+  });
 });
