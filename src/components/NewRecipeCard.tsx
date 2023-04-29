b4b4e58315f650834381a8a71dfedf0070098de3
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-04-29 17:14:06 +0300

    feat(i18n): translate new recipe card

--- src/components/NewRecipeCard.tsx
+++ src/components/NewRecipeCard.tsx
@@ -1,10 +1,12 @@
 import { PlusIcon } from "@radix-ui/react-icons";
 import styles from "./NewRecipeCard.module.css";
 import { Link } from "./link/Link";
+import { useTranslation } from "next-i18next";
 
 export const NewRecipeCard = () => {
+  const { t } = useTranslation("home");
   return <Link className={styles.link} href="/recipe/new">
     <PlusIcon width={24} height={24} />
-    Create new recipe
+    {t("newRecipeButtonLong")}
   </Link>;
 };
