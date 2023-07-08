33d61aaa55be60826d8e29ac7e8cfb021e46f156
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-07-08 23:40:07 +0300

    feat: add env variable for using SSL for S3

--- src/s3.ts
+++ src/s3.ts
@@ -5,7 +5,7 @@ export const DEFAULT_BUCKET_NAME = process.env.S3_BUCKET_NAME ?? "";
 export const s3 = new Minio.Client({
   endPoint: process.env.S3_ENDPOINT ?? "",
   port: process.env.S3_PORT ? parseInt(process.env.S3_PORT) : 9000,
-  useSSL: false,
+  useSSL: process.env.S3_USE_SSL === "true" ? true : false,
   accessKey: process.env.S3_ACCESS_KEY_ID ?? "",
   secretKey: process.env.S3_SECRET_ACCESS_KEY ?? "",
 });
