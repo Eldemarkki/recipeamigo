ce1a410093a71ab852a8402a07d894566b55df44
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-05 23:05:13 +0300

    feat: add title to settings page

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,21 +1,30 @@
 import { LanguageSelector } from "../../components/languageSelector/LanguageSelector";
 import { PageWrapper } from "../../components/misc/PageWrapper";
 import ThemeToggle from "../../components/themeToggle/ThemeToggle";
+import config from "../../config";
 import styles from "./index.module.css";
 import type { GetServerSideProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
 
 export default function SettingsPage() {
   const { t } = useTranslation();
 
   return (
-    <PageWrapper title={t("settings:settingsTitle")}>
-      <div className={styles.container}>
-        <ThemeToggle />
-        <LanguageSelector />
-      </div>
-    </PageWrapper>
+    <>
+      <Head>
+        <title>
+          {t("settings:settingsTitle")} | {config.APP_NAME}
+        </title>
+      </Head>
+      <PageWrapper title={t("settings:settingsTitle")}>
+        <div className={styles.container}>
+          <ThemeToggle />
+          <LanguageSelector />
+        </div>
+      </PageWrapper>
+    </>
   );
 }
 
