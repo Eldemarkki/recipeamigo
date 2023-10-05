2a33267e17090983ab381e213bf5d9dc5340ec3f
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-05 23:10:53 +0300

    feat: add title to 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,16 +1,25 @@
+import config from "../config";
 import styles from "./errorPage.module.css";
 import type { GetStaticProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
 
 export default function NotFoundPage() {
   const { t } = useTranslation("500");
 
   return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>{t("title")}</h1>
-      <p className={styles.description}>{t("description")}</p>
-    </div>
+    <>
+      <Head>
+        <title>
+          {t("title")} | {config.APP_NAME}
+        </title>
+      </Head>
+      <div className={styles.page}>
+        <h1 className={styles.title}>{t("title")}</h1>
+        <p className={styles.description}>{t("description")}</p>
+      </div>
+    </>
   );
 }
 
