722b415da21a2ad4d439f2280d265e9cabd7be51
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-05 22:44:27 +0300

    feat: add title to collection page

--- src/pages/collections/[id]/index.tsx
+++ src/pages/collections/[id]/index.tsx
@@ -3,12 +3,14 @@ import { RecipeCardGrid } from "../../../components/RecipeCardGrid";
 import { Link } from "../../../components/link/Link";
 import { PageWrapper } from "../../../components/misc/PageWrapper";
 import { VisibilityText } from "../../../components/misc/VisibilityText";
+import config from "../../../config";
 import { collectionPageDataLoader } from "../../../dataLoaders/collections/collectionPageDataLoader";
 import { createPropsLoader } from "../../../dataLoaders/loadProps";
 import styles from "./index.module.css";
 import { PersonIcon } from "@radix-ui/react-icons";
 import type { InferGetServerSidePropsType } from "next";
 import { useTranslation } from "next-i18next";
+import Head from "next/head";
 
 export default function CollectionPage({
   collection,
@@ -17,49 +19,56 @@ export default function CollectionPage({
   const { t } = useTranslation(["common", "collections"]);
 
   return (
-    <PageWrapper
-      titleRow={
-        <div className={styles.topRow}>
-          <h1>{collection.name}</h1>
-          {isOwner && (
-            <LinkButton href={`/collections/${collection.id}/edit`}>
-              {t("actions.edit")}
-            </LinkButton>
-          )}
+    <>
+      <Head>
+        <title>
+          {collection.name} | {config.APP_NAME}
+        </title>
+      </Head>
+      <PageWrapper
+        titleRow={
+          <div className={styles.topRow}>
+            <h1>{collection.name}</h1>
+            {isOwner && (
+              <LinkButton href={`/collections/${collection.id}/edit`}>
+                {t("actions.edit")}
+              </LinkButton>
+            )}
+          </div>
+        }
+      >
+        <div className={styles.infoSection}>
+          {collection.description && <p>{collection.description}</p>}
+          <div className={styles.infoRow}>
+            <VisibilityText
+              type="collection"
+              visibility={collection.visibility}
+            />
+            <span>{"\u2022"}</span>
+            <Link
+              href={`/user/${collection.user.username}`}
+              icon={<PersonIcon />}
+            >
+              {collection.user.username}
+            </Link>
+            <span>{"\u2022"}</span>
+            <span>
+              {t("collections:recipeCount", {
+                count: collection.RecipesOnCollections.length,
+              })}
+            </span>
+          </div>
         </div>
-      }
-    >
-      <div className={styles.infoSection}>
-        {collection.description && <p>{collection.description}</p>}
-        <div className={styles.infoRow}>
-          <VisibilityText
-            type="collection"
-            visibility={collection.visibility}
+        <hr />
+        {collection.RecipesOnCollections.length ? (
+          <RecipeCardGrid
+            recipes={collection.RecipesOnCollections.map((r) => r.recipe)}
           />
-          <span>{"\u2022"}</span>
-          <Link
-            href={`/user/${collection.user.username}`}
-            icon={<PersonIcon />}
-          >
-            {collection.user.username}
-          </Link>
-          <span>{"\u2022"}</span>
-          <span>
-            {t("collections:recipeCount", {
-              count: collection.RecipesOnCollections.length,
-            })}
-          </span>
-        </div>
-      </div>
-      <hr />
-      {collection.RecipesOnCollections.length ? (
-        <RecipeCardGrid
-          recipes={collection.RecipesOnCollections.map((r) => r.recipe)}
-        />
-      ) : (
-        <p>{t("collections:emptyCollection")}</p>
-      )}
-    </PageWrapper>
+        ) : (
+          <p>{t("collections:emptyCollection")}</p>
+        )}
+      </PageWrapper>
+    </>
   );
 }
 
