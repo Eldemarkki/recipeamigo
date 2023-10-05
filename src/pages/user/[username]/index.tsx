29e272e0a4dc9be9f4125eed2bdd195cbaee3b62
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-05 23:06:15 +0300

    feat: add title to user page

--- src/pages/user/[username]/index.tsx
+++ src/pages/user/[username]/index.tsx
@@ -1,11 +1,13 @@
 import { RecipeCardGrid } from "../../../components/RecipeCardGrid";
 import { CollectionsList } from "../../../components/collections/CollectionsList";
 import { PageWrapper } from "../../../components/misc/PageWrapper";
+import config from "../../../config";
 import { createPropsLoader } from "../../../dataLoaders/loadProps";
 import { userPageDataLoader } from "../../../dataLoaders/user/userPageDataLoader";
 import styles from "./index.module.css";
 import type { InferGetServerSidePropsType } from "next";
 import { useTranslation } from "next-i18next";
+import Head from "next/head";
 
 export default function UserPage({
   user,
@@ -13,34 +15,41 @@ export default function UserPage({
   const { t } = useTranslation("userPage");
 
   return (
-    <PageWrapper title={user.username} mainClass={styles.content}>
-      <section className={styles.section}>
-        <h2>{t("recipesTitle")}</h2>
-        {user.recipes.length > 0 ? (
-          <>
-            <p>{t("recipeCount", { count: user.recipes.length })}</p>
-            <RecipeCardGrid recipes={user.recipes} />
-          </>
-        ) : (
-          <p>{t("noRecipes")}</p>
-        )}
-      </section>
-      <section className={styles.section}>
-        <h2>{t("collections.title")}</h2>
-        {user.recipeCollections.length > 0 ? (
-          <>
-            <p>
-              {t("collections.collectionCount", {
-                count: user.recipeCollections.length,
-              })}
-            </p>
-            <CollectionsList collections={user.recipeCollections} />
-          </>
-        ) : (
-          <p>{t("collections.noCollections")}</p>
-        )}
-      </section>
-    </PageWrapper>
+    <>
+      <Head>
+        <title>
+          {user.username} | {config.APP_NAME}
+        </title>
+      </Head>
+      <PageWrapper title={user.username} mainClass={styles.content}>
+        <section className={styles.section}>
+          <h2>{t("recipesTitle")}</h2>
+          {user.recipes.length > 0 ? (
+            <>
+              <p>{t("recipeCount", { count: user.recipes.length })}</p>
+              <RecipeCardGrid recipes={user.recipes} />
+            </>
+          ) : (
+            <p>{t("noRecipes")}</p>
+          )}
+        </section>
+        <section className={styles.section}>
+          <h2>{t("collections.title")}</h2>
+          {user.recipeCollections.length > 0 ? (
+            <>
+              <p>
+                {t("collections.collectionCount", {
+                  count: user.recipeCollections.length,
+                })}
+              </p>
+              <CollectionsList collections={user.recipeCollections} />
+            </>
+          ) : (
+            <p>{t("collections.noCollections")}</p>
+          )}
+        </section>
+      </PageWrapper>
+    </>
   );
 }
 
