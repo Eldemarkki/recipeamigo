d7e0039e3e8606b8cd22c472aecf76ef0339aac2
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-23 19:35:29 +0300

    feat: disable image optimization

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: (process.env.IMAGE_HOSTS ?? "").split(" "),
+    // We can't use optimized images because they are loaded with a presigned S3 URL,
+    // so the link is always different and the cache is never used, even though the
+    // image is the same.
+    unoptimized: true,
   },
   i18n,
   experimental: {
