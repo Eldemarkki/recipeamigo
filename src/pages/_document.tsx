488a31f03d4dcf96c88ad564ff7f2ce34678f1c4
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2023-10-25 22:14:47 +0300

    feat: add env variables for Plausible analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,20 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const plausibleDataDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DATA_DOMAIN;
+const plausibleSrc = process.env.NEXT_PUBLIC_PLAUSIBLE_SRC;
+
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        {plausibleDataDomain && plausibleSrc && (
+          <script
+            defer
+            data-domain={plausibleDataDomain}
+            src={plausibleSrc}
+          ></script>
+        )}
       </Head>
       <body>
         <Main />
